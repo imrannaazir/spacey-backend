2922e86a86956a31ecd207b77701fd13c06a8d77
imrannaazir <imrannaaziremon@gmail.com>
2022-05-06 23:07:33 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const jwt = require('jsonwebtoken');
 app.use(cors());
 app.use(express.json());
 
+// jwt function
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    else {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: 'access forbidden' })
+            }
+            else {
+                req.decoded = decoded
+                next()
+            };
+        });
+    }
+}
+
+
 //connect with mongodb
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -17,34 +38,36 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-/* client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-}); */
-
 //start async function
 (async function run() {
     try {
         await client.connect();
         const rocketCollection = client.db("spacey").collection("rockets")
 
-        //get all rockets api
-        app.get('/rockets', async (req, res) => {
-            const email = req?.query?.email;
-            if (email) {
-                const query = { email: email }
-                const cursor = rocketCollection.find(query)
-                const rockets = await cursor.toArray()
-                res.send(rockets)
-            }
-            else {
-                const query = {};
+
+        //get my rockets api
+        app.get('/myrockets', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+
+            if (email === decodedEmail) {
+                const query = { email: email };
                 const cursor = rocketCollection.find(query);
                 const rockets = await cursor.toArray();
-                res.send(rockets)
+                res.send(rockets);
             }
+            else {
+                res.status(403).send({ message: 'access forbidden' })
+            };
 
+        })
+
+        //get all rockets api
+        app.get('/rockets', async (req, res) => {
+            const query = {};
+            const cursor = rocketCollection.find(query);
+            const rockets = await cursor.toArray();
+            res.send(rockets)
         });
         //get one rocket api
         app.get('/rockets/:id', async (req, res) => {
@@ -54,6 +77,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
             res.send(rocket)
         });
+
+        // get access token api 
+        app.post('/login', async (req, res) => {
+            const email = req.body;
+            const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ accessToken })
+        })
 
         //Post one rockets api
         app.post('/rockets', async (req, res) => {
@@ -74,7 +104,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         app.put('/rockets/:id', async (req, res) => {
             const updatedRocket = req.body;
             const id = req.params.id;
-            console.log(updatedRocket);
             const query = { _id: ObjectId(id) };
             const option = { upsert: true };
             const updatedDoc = {
@@ -109,4 +138,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('The voice of express is sweet.');
 })
-
